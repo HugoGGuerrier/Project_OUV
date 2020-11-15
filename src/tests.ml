f1f283cc7fde11e92afa894ff9c4ddfefb718d6c
Hugo Guerrier <hugogguerrier@gmail.com>
2020-11-15 17:42:04 +0100

    Add tests for Q1.2

--- src/tests.ml
+++ src/tests.ml
@@ -2,44 +2,57 @@ open Abr
 
 
 let test_extraction_alea () = 
-    (* listes l et p banales *)
-    let l10 = [1;2;3] and p10 = [] in
-    let (fl10, fp10) = extraction_alea l10 p10 in
-    assert (List.length fl10 = List.length l10 - 1) ;
-    assert (List.length fp10 = List.length p10 + 1) ;
-    
-    let l11 = [1;2;3;4;5] and p11 = [10;20;30] in
-    let (fl11, fp11) = extraction_alea l11 p11 in
-    assert (List.length fl11 = List.length l11 - 1) ;
-    assert (List.length fp11 = List.length p11 + 1) ;
-    
-    let l12 = [1;2;3;4] and p12 = [10] in
-    let (fl12, fp12) = extraction_alea l12 p12 in
-    assert (List.length fl12 = List.length l12 - 1) ;
-    assert (List.length fp12 = List.length p12 + 1) ;
-    
-    let l13 = [1;2;3;4;5;6;7;8;9] and p13 = [10;20] in
-    let (fl13, fp13) = extraction_alea l13 p13 in
-    assert (List.length fl13 = List.length l13 - 1) ;
-    assert (List.length fp13 = List.length p13 + 1) ;
-    
-    (* liste l vide *)
-    let l2 = [] and p2 = [1;2] in
-    let (fl2, fp2) = extraction_alea l2 p2 in
-    assert (List.length fl2 = 0) ;
-    assert (List.length fp2 = List.length p2) ;
-    
-    (* listes vides *)
-    let l3 = [] and p3 = [] in
-    let (fl3, fp3) = extraction_alea l3 p3 in
-    assert (List.length fl3 = 0) ;
-    assert (List.length fp3 = 0) ;
-    ()
+  (* listes l et p banales *)
+  let l10 = [1;2;3] and p10 = [] in
+  let (fl10, fp10) = extraction_alea l10 p10 in
+  assert (List.length fl10 = List.length l10 - 1) ;
+  assert (List.length fp10 = List.length p10 + 1) ;
+  
+  let l11 = [1;2;3;4;5] and p11 = [10;20;30] in
+  let (fl11, fp11) = extraction_alea l11 p11 in
+  assert (List.length fl11 = List.length l11 - 1) ;
+  assert (List.length fp11 = List.length p11 + 1) ;
+  
+  let l12 = [1;2;3;4] and p12 = [10] in
+  let (fl12, fp12) = extraction_alea l12 p12 in
+  assert (List.length fl12 = List.length l12 - 1) ;
+  assert (List.length fp12 = List.length p12 + 1) ;
+  
+  let l13 = [1;2;3;4;5;6;7;8;9] and p13 = [10;20] in
+  let (fl13, fp13) = extraction_alea l13 p13 in
+  assert (List.length fl13 = List.length l13 - 1) ;
+  assert (List.length fp13 = List.length p13 + 1) ;
+  
+  (* liste l vide *)
+  let l2 = [] and p2 = [1;2] in
+  let (fl2, fp2) = extraction_alea l2 p2 in
+  assert (List.length fl2 = 0) ;
+  assert (List.length fp2 = List.length p2) ;
+  
+  (* listes vides *)
+  let l3 = [] and p3 = [] in
+  let (fl3, fp3) = extraction_alea l3 p3 in
+  assert (List.length fl3 = 0) ;
+  assert (List.length fp3 = 0) ;
+  ()
+
+
+let test_gen_permutation () =
+  (* n quelconque *)
+  assert ((List.fold_left ( + ) 0 (gen_permutation 5)) = (1 + 2 + 3 + 4 + 5));
+  assert ((List.fold_left ( + ) 0 (gen_permutation 3)) = (1 + 2 + 3));
+  assert ((List.fold_left ( + ) 0 (gen_permutation 10)) = (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10));
+  assert ((List.fold_left ( + ) 0 (gen_permutation 1)) = (1));
+
+  (* n nul *)
+  assert ((List.length (gen_permutation 0)) = 0);
+  ()
 
 
 let run_tests () =
   let liste_tests = [
-        ("Q1.1) Extraction aléatoire dans une liste", test_extraction_alea)
+      ("Q1.1 - Extraction aléatoire dans une liste", test_extraction_alea);
+      ("Q1.2 - Génération d'une liste avec l’algorithme de shuffle de Fisher-Yates", test_gen_permutation)
     ]
   in
   List.iteri
