44fe857cc46fbcd4199c39448ddb41825a753f12
Emilie Siau <emilie.siau@gmail.com>
2020-11-15 16:43:28 +0100

    Q1.1

--- src/abr.ml
+++ src/abr.ml
@@ -1,1 +1,12 @@
-let test_func () = print_endline "Test OK !"; Bbr.autre ();;
+
+let extraction_alea l p = 
+    match l with 
+    | [] -> (l, p)
+    | _ ->
+        let rand = Random.int (List.length l) in
+        let arr = Array.of_list l in
+        let arr1 = Array.sub arr 0 rand in
+        let arr2 = Array.sub arr (rand + 1) (List.length l - rand - 1) in
+        let l_res = Array.to_list (Array.append arr1 arr2) in
+        (l_res, (List.nth l rand)::p)
+
--- src/tests.ml
+++ src/tests.ml
@@ -1,4 +1,31 @@
 open Abr
-;;
-test_func ()
-;;
+
+
+let test_extraction_alea () = 
+    let l = [1;2;3]
+    and p = []
+    and (lf, pf) = extraction_alea [1;2;3] [] in
+    assert (List.length lf = List.length l - 1) ;
+    assert (List.length pf = List.length p + 1) ;
+    ()
+
+
+let run_tests () =
+  let liste_tests = [
+        ("Q1.1) Extraction aléatoire dans une liste", test_extraction_alea)
+    ]
+  in
+  List.iteri
+    (fun i (nom_test, f_test) ->
+      Format.printf "--- Test #%d - %s:\t" (i + 1) nom_test ;
+      try
+        f_test () ;
+        Format.printf "\027[32mOK\n\027[39m"
+      with exn ->
+        Format.printf "\027[31mErreur - %s\n\027[39m" (Printexc.to_string exn))
+    liste_tests
+    
+
+(* Main *)
+let () =
+ 	run_tests ()
